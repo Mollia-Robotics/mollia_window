42f6736d2bc478ca37884e97325ef74b13cb7f1a
szabi <szabi@mollia.hu>
2022-04-11 16:20:39 +0300

    mollia_window implementation

---
+++ mollia_window/common.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <Python.h>
+
+#define UNICODE
+#include <Windows.h>
+
+#ifndef BUILD_COMMIT
+#define BUILD_COMMIT unknown
+#endif
+
+#define xstr(s) str(s)
+#define str(s) #s
+
+#define xstr(x) str(x)
+#define TRACE() __FUNCTION__ " (" __FILE__ ":" xstr(__LINE__) ")"
+
+#define WND_STYLE (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX)
+
+inline POINT window_size(POINT size, bool menu) {
+    /*
+        Shortcut to calculate the width and height of the window
+    */
+    RECT rect = {0, 0, size.x, size.y};
+    AdjustWindowRect(&rect, WND_STYLE, menu);
+    return {rect.right - rect.left, rect.bottom - rect.top};
+}
